f6fe77abff55d9b42a412854b7e86b0553da464d
gilmartaj <44650363+gilmartaj@users.noreply.github.com>
2020-03-02 11:19:57 +0000

    Update insertion_sort.d

--- insertion_sort.d
+++ insertion_sort.d
@@ -1,16 +1,16 @@
 void insertionSort(int[] v)
 {
-	for(int i = 1; i < v.length; i++)
-	{
-		int chave = v[i];
-		int j = i-1;
-		while(j >= 0 && v[j] > chave)
-		{
-			v[j+1] = v[j];
-			j--;
-		}
-		v[j+1] = chave;
-	}
+    for(int i = 1; i < v.length; i++)
+    {
+        int chave = v[i];
+	int j = i - 1;
+        while(j >= 0 && v[j] > chave)
+        {
+            v[j + 1] = v[j];
+            --j;
+        }
+        v[j + 1] = chave;
+    }
 }
 
 // usa foreach ao invés de for no laço externo
