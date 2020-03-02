30f4f61c6087a7bdaf5e0d8c605345275da0d098
gilmartaj <44650363+gilmartaj@users.noreply.github.com>
2020-03-02 11:27:59 +0000

    Update insertion_sort_reverse.d

--- insertion_sort_reverse.d
+++ insertion_sort_reverse.d
@@ -3,12 +3,12 @@ void insertionSortReverse(int[] v)
     for(int i = cast(int)(v.length - 2); i >= 0; i--)
     {
         int chave = v[i];
-        int j = i+1;
+        int j = i + 1;
         while(j < v.length && v[j] > chave)
         {
-            v[j-1] = v[j];
-            j++;
+            v[j - 1] = v[j];
+            ++j;
         }
-        v[j-1] = chave;
+        v[j - 1] = chave;
     }
 }
